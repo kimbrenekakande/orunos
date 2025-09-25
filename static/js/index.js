567237cb29864a67f3f3651a66cf19890fc029de
kimbrene kakande <kimbrenekakande@gmail.com>
2025-09-25 16:16:16 +0300

    humberger close button

--- static/js/index.js
+++ static/js/index.js
@@ -1,16 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const menuBtn = document.getElementById('menu-btn');
+    const menuBtnOpen = document.getElementById('menu-btn-open');
+    const menuBtnClose = document.getElementById('menu-btn-close');
     const fullscreenNav = document.getElementById('fullscreen-nav');
-    const cross = document.getElementById('cross');
-    const burger = document.getElementById('burger');
 
-    menuBtn.addEventListener('click', function () {
-        fullscreenNav.classList.toggle('hidden');
-        cross.classList.toggle('hidden');
-        if (fullscreenNav.classList.contains('hidden')) {
-            burger.classList.remove('hidden');
-        } else {
-            burger.classList.add('hidden');
-        }
+    menuBtnOpen.addEventListener('click', function () {
+        fullscreenNav.classList.remove('hidden');
+    });
+
+    menuBtnClose.addEventListener('click', function () {
+        fullscreenNav.classList.add('hidden');
     });
 });

dccaff7f4cae7bbf81a0f0f0e9d721926133c80c
kimbrene kakande <kimbrenekakande@gmail.com>
2025-09-25 11:00:56 +0300

    ckeditor responsiveness on mobile

--- project/ckeditor5.py
+++ project/ckeditor5.py
@@ -25,7 +25,7 @@ customColorPalette = [
     },
 ]
 
-CKEDITOR_5_CUSTOM_CSS = 'path_to.css'  # optional
+CKEDITOR_5_CUSTOM_CSS = 'css/ck.css'  # Custom CSS for CKEditor
 CKEDITOR_5_FILE_STORAGE = "path_to_storage.CustomStorage"  # optional
 CKEDITOR_5_CONFIGS = {
     'default': {

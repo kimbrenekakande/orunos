ba75fac9ea33966075a2c0eccabba48b2585f24f
Kimbrene Kakande <kimbrenekakande@gmail.com>
2025-10-04 13:37:40 +0300

    prompt input

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,12 +1,16 @@
+"use client"
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset } from "@/components/ui/sidebar";
+import { PromptBox } from "@/components/ui/chatgpt-prompt-input";
 
 export default function Page() {
 	return (
 		<SidebarInset>
 			<SiteHeader />
-			<div className="flex flex-1 flex-col items-center justify-center">
-        <h1>Dashboard</h1>
+			<div className="flex flex-1 flex-col items-center justify-center h-full w-full">
+        <div className="w-[90%] mx-auto md:w-[50%]">
+          <PromptBox />
+        </div>
       </div>
 		</SidebarInset>
 	);

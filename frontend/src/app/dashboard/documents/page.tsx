43c23b02372756b80aebc7828519c35a3deaa92f
Kimbrene Kakande <kimbrenekakande@gmail.com>
2025-10-06 11:46:04 +0300

    login page nextjs

--- frontend/src/app/dashboard/documents/page.tsx
+++ frontend/src/app/dashboard/documents/page.tsx
@@ -1,12 +1,14 @@
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset } from "@/components/ui/sidebar";
+import data from "@/lib/data.json"
+import { DataTable } from "@/components/data-table"
 
 export default function Page() {
 	return (
 		<SidebarInset>
 			<SiteHeader />
 			<div className="flex flex-1 flex-col items-center justify-center">
-        <h1>Documents List</h1>
+        <DataTable data={data} />
       </div>
 		</SidebarInset>
 	);

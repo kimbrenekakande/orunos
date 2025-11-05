d81138f21ff926c159dbe69410b1c9da15c3a783
Kimbrene Kakande <kimbrenekakande@gmail.com>
2025-11-05 16:24:13 +0300

    hero image

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,10 @@ const App = () => {
   return (
     <div className='h-screen w-screen bg-amber-700'>
       <CardNav items={items} baseColor="#fff" menuColor="#000" buttonBgColor="#111" buttonTextColor="#fff" ease="power3.out" />
-      <div id="hero" className="min-h-screen w-full bg-orange-500 relative flex flex-col items-center justify-center" style={{backgroundImage: 'url(/images/boat.jpg)', backgroundSize: 'cover', backgroundPosition: 'center'}}>
+      <div id="hero" className="min-h-screen w-full bg-orange-500 relative flex flex-col items-center justify-center" style={{backgroundImage: 'url(/images/tree.jpg)', backgroundSize: 'cover', backgroundPosition: 'center'}}>
         <div id="call-to-action" className="flex flex-col gap-6 items-center justify-center">
           <h1 className="text-2xl text-white">Your AI Study Buddy</h1>
-          <h2 className="text-4xl text-black font-crimson-text motion-preset-focus motion-duration-1000 text-center">WRITE CITE LEARN</h2>
+          <h2 className="text-4xl text-white font-crimson-text motion-preset-focus motion-duration-1000 text-center">WRITE CITE LEARN</h2>
           <p className="text-white text-center">Designed to to help students complete Coursework,to focus on learning.</p>
 
           <Link href="/dashboard" className="h-10 w-32 bg-amber-600 hover:bg-amber-700 flex justify-center items-center rounded-sm text-white">GET STARTED</Link>

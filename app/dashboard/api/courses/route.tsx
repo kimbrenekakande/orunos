7003e84f0fee67ae554ef8c8db1a76a1582acf8d
Kimbrene Kakande <kimbrenekakande@gmail.com>
2025-11-10 15:53:35 +0300

    git prisma installed

---
+++ app/dashboard/api/courses/route.tsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+// export async function GET() { 
+//   works = await prisma.coursework.FindMany();
+// }
+
+import React from 'react'
+
+function sampled() {
+  return (
+    <div>sampled</div>
+  )
+}
+
+export default sampled
--- app/dashboard/test/page.jsx
+++ app/dashboard/test/page.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import Link from "next/link";
+import { prisma } from "@/prisma/db";
 
-function test() {
-  return (
-    <div className='h-20 bg-red-600'>
-      <h1>test</h1>
-    </div>
-  )
+export default async function name() {
+	const works = await prisma.coursework.findMany();
+	return (
+		<div className="h-20 bg-red-600">
+			<h1>All Courses</h1>
+			<ul>
+				{works.map((w) => (
+					<Link key={w.id} href={"#"}>
+						<li>{w.question}</li>
+					</Link>
+				))}
+			</ul>
+		</div>
+	);
 }
-
-export default test

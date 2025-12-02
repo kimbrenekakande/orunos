0d7e6d164dde7d71b8ab0c095fd362750f0799eb
Kimbrene Kakande <kimbrenekakande@gmail.com>
2025-12-02 22:30:08 +0300

    refining doc creator

--- lib/actions/creator.ts
+++ lib/actions/creator.ts
@@ -1,15 +1,41 @@
-"use server";
+'use server'
 
 import { prisma } from "@/lib/prisma-client";
 import { redirect } from "next/navigation";
+import { serverSession } from "@/lib/server-session";
 
-export async function addShit(formData: FormData) {
-	const doctype = formData.get("doctype") as string;
-	const newPaper = await prisma.document.create({
-		data: {
-			question: formData.get("qn") as string,
-			answer: formData.get("ans") as string,
-		},
-	});
-	redirect(`/dashboard/${doctype}/editor/${newPaper.id}`);
+export async function startCreation(formData: FormData) {
+  const session = await serverSession();
+  const user = session?.user;
+  if (!user) throw new Error("User not authenticated");
+
+  
+  const tempType = formData.get("doctype") as string;
+  if (!tempType) throw new Error("Document type is required");
+
+  
+  const docType = await prisma.docType.findUnique({
+    where: {
+      type: tempType
+    }
+  });
+  if (!docType) throw new Error("Invalid document type");
+
+  
+  const question = formData.get("qns") as string;
+  const answer = formData.get("ans") as string;
+  if (!question) throw new Error("Question and answer are required");
+  
+
+  const newDoc = await prisma.document.create({
+    data: {
+      cost: Number(docType.price),
+      question,
+      answer,
+      userId: user.id,
+      docTypeId: docType.type
+    },
+  });
+  
+  redirect(`/dashboard/${tempType}/editor/${newDoc.id}`);
 }
--- shit.tsx
+++ shit.tsx
@@ -1,14 +1,9 @@
-import { generateText } from 'ai';
-import { createDeepSeek } from '@ai-sdk/deepseek';
+import { prisma } from "./lib/prisma-client" 
 
-const deepseek = createDeepSeek({
-  apiKey: process.env.DEEPSEEK_API_KEY,
-});
+const type =await prisma.docType.findUnique({
+    where  : {
+      type : 'coursework'
+    },
+  })
 
-const { text } = await generateText({
-  model: deepseek('deepseek-chat'),
-  system: 'You are a super bright college student and you are going to return the outline for a coursework question',
-  prompt: 'Define Africa\'s future if it used cultural identity in its technology development?',
-});
-
-console.log(text);
+  console.log(type)

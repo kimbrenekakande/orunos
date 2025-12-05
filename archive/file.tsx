ae7ed661d9db47abc67b404b863f1f683691c63e
Kimbrene Kakande <kimbrenekakande@gmail.com>
2025-12-05 15:57:20 +0300

    stop tracking archive directory

--- archive/file.tsx
+++
@@ -1,8 +0,0 @@
-
-// reading an md file into code
-import { readFile } from "fs/promises";
-import path from "path";
-// import { useState } from "react";
-const filepath = path.join( './study.md')
-
-export const file = await readFile(filepath, 'utf-8')

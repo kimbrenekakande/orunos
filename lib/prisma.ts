715ba0118676be32de1229384d9a4950c5486d34
Kimbrene Kakande <kimbrenekakande@gmail.com>
2025-12-05 19:53:27 +0300

    reconfiguring to accommodate turso db

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,9 @@
-import {PrismaClient} from "@prisma/client"
-import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { PrismaClient } from "@prisma/client";
+import { PrismaLibSql } from "@prisma/adapter-libsql";
 
-const adapter = new PrismaLibSQL({
-  url: `${process.env.TURSO_DATABASE_URL}`,
-  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
-})
-const prisma = new PrismaClient({ adapter })
-export default prisma
+const adapter = new PrismaLibSql({
+	url: `${process.env.TURSO_DATABASE_URL}`,
+	authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+});
+const prisma = new PrismaClient({ adapter });
+export default prisma;

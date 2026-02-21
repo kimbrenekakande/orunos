6ebfe609984db4b8713e8ec29412b927a41d1dd6
Kimbrene Kakande <kimbrenekakande@gmail.com>
2026-02-21 11:49:29 +0300

    remove base url from better auth lib instance

--- apps/web/lib/auth.tsx
+++ apps/web/lib/auth.tsx
@@ -6,8 +6,6 @@ import  prisma  from "@/lib/prisma";
 import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
-  baseURL: process.env.BETTER_AUTH_URL,
-  
 	database: prismaAdapter(prisma, {
 		provider: "sqlite", // or "mysql", "postgresql", ...etc
 	}),

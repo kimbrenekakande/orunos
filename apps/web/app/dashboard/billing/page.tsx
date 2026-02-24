1b956bdabdf39a5ce1a73cbfce6faa2e00679cca
Kimbrene Kakande <kimbrenekakande@gmail.com>
2026-02-24 13:35:00 +0300

    nav-user redesign and removal of timestaps from institute and docs (couldnt update content in their tables manually)

---
+++ apps/web/app/dashboard/billing/page.tsx
@@ -0,0 +1,7 @@
+export default function Billing() {
+  return (
+    <div>
+      <h2>Billing</h2>
+    </div>
+  )
+}
--- apps/web/components/dashboard/site-header.tsx
+++ apps/web/components/dashboard/site-header.tsx
@@ -6,17 +6,17 @@ import { ModeToggle} from "@/components/ui/theme-toggle"
 import { usePathname } from "next/navigation"
 import { NavUser } from "./nav-user"
 
-export function SiteHeader() { 
+export function SiteHeader() {
   const kubo = usePathname().includes('/editor/')
-  
+
   const user =  {
     name: "kakande",
     email: "kimbrene@gmail.com",
     avatar: "/images/tree.jpg",
   }
-  
+
   if (kubo) return null
-  
+
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full justify-between px-8 mt-16">
@@ -24,10 +24,8 @@ export function SiteHeader() {
         <div className="flex justify-between ">
           <ModeToggle/>
         </div>
-        
-        <div className="bg-blue-600s">
-          <NavUser user={user} />
-        </div>  
+
+        <NavUser user={user} />
       </div>
     </header>
   )

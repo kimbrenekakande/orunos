d95c33dab6705039ecbeee64715d5c93db1f711a
Kimbrene Kakande <kimbrenekakande@gmail.com>
2026-03-10 20:31:47 +0300

    deactivate links for document types that are not ready for release

--- apps/web/lib/templates.ts
+++ apps/web/lib/templates.ts
@@ -2,51 +2,61 @@ export const templates = [
 	{
 		name: "Coursework",
 		type: "coursework",
-		image: "/images/largepreview.png",
+    image: "/images/largepreview.png",
+		ready : true
+  },
+  {
+		name: "Essay",
+		type: "essay",
+		image : "/images/largepreview.png",
+		ready : true
 	},
 	{
 		name: "Research Paper",
 		type: "research",
-		image: "/images/largepreview.png"
-	},
-	{
-		name: "Essay",
-		type: "essay",
-		image : "/images/largepreview.png"
+		image: "/images/largepreview.png",
+		ready : false
 	},
 	{
 		name: "Lab Report",
 		type: "lab",
-		image: "/images/largepreview.png"
+    image: "/images/largepreview.png",
+		ready : false
 	},
 	{
 		name: "Thesis",
 		type: "thesis",
-		image: "/images/largepreview.png"
+    image: "/images/largepreview.png",
+		ready : false
 	},
-	{
-		name: "Dissertation",
-		type: "dissertation",
-		image:"/images/largepreview.png"
-	},
+  {
+    name: "Dissertation",
+    type: "dissertation",
+    image: "/images/largepreview.png",
+    ready: false
+  },
 	{
 		name: "Case Study",
 		type: "case-study",
-		image:"/images/largepreview.png"
+    image: "/images/largepreview.png",
+		ready : false
 	},
 	{
 		name: "Literature Review",
 		type: "literature-review",
-		image:"/images/largepreview.png"
+    image: "/images/largepreview.png",
+		ready : false
 	},
 	{
 		name: "Proposal",
 		type: "proposal",
-		image:"/images/largepreview.png"
+    image: "/images/largepreview.png",
+		ready : false,
 	},
 	{
 		name: "Presentation",
 		type: "presentation",
-		image:"/images/largepreview.png"
+    image: "/images/largepreview.png",
+		ready : false
 	},
 ];
